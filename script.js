468fdc9a746e9701dd688a5c0e26aeb3ff6626ed
Rohit Parchure <114169315+Rohit-Parchure@users.noreply.github.com>
2023-01-10 18:43:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let userbox = document.getElementById("userbox");
+
+function openinfo(){
+    userbox.classList.remove("hide-details");
+}
+
+function closeinfo(){
+    userbox.classList.add("hide-details");
+}
